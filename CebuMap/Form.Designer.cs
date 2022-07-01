a37aaccc6f9a766ecc32ccefb9394aeeb3cc8215
John James Libron <johnjames.libron@gmail.com>
2022-07-01 13:51:30 +0800

    Initial commit

---
+++ CebuMap/Form.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CebuMap
+{
+    partial class FormMap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.panelGraph = new System.Windows.Forms.Panel();
+            this.lblStartVertex = new System.Windows.Forms.Label();
+            this.lblTargetVertex = new System.Windows.Forms.Label();
+            this.cboSearch = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(1070, 191);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(163, 42);
+            this.btnDisplay.TabIndex = 0;
+            this.btnDisplay.Text = "Display Points";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.BtnDisplay_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Enabled = false;
+            this.btnRemove.Location = new System.Drawing.Point(1070, 239);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(163, 42);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.BtnRemove_Click);
+            // 
+            // panelGraph
+            // 
+            this.panelGraph.BackgroundImage = global::CebuMap.Properties.Resources.map;
+            this.panelGraph.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelGraph.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelGraph.Location = new System.Drawing.Point(12, 12);
+            this.panelGraph.Name = "panelGraph";
+            this.panelGraph.Size = new System.Drawing.Size(1048, 648);
+            this.panelGraph.TabIndex = 0;
+            // 
+            // lblStartVertex
+            // 
+            this.lblStartVertex.AutoSize = true;
+            this.lblStartVertex.Location = new System.Drawing.Point(1066, 67);
+            this.lblStartVertex.Name = "lblStartVertex";
+            this.lblStartVertex.Size = new System.Drawing.Size(98, 20);
+            this.lblStartVertex.TabIndex = 8;
+            this.lblStartVertex.Text = "Start Vertex:";
+            // 
+            // lblTargetVertex
+            // 
+            this.lblTargetVertex.AutoSize = true;
+            this.lblTargetVertex.Location = new System.Drawing.Point(1066, 96);
+            this.lblTargetVertex.Name = "lblTargetVertex";
+            this.lblTargetVertex.Size = new System.Drawing.Size(109, 20);
+            this.lblTargetVertex.TabIndex = 9;
+            this.lblTargetVertex.Text = "Target Vertex:";
+            // 
+            // cboSearch
+            // 
+            this.cboSearch.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSearch.Enabled = false;
+            this.cboSearch.FormattingEnabled = true;
+            this.cboSearch.Items.AddRange(new object[] {
+            "Breadth-First Search",
+            "Depth-First Search",
+            "Greedy Best-First Search",
+            "A* Search"});
+            this.cboSearch.Location = new System.Drawing.Point(1070, 12);
+            this.cboSearch.Name = "cboSearch";
+            this.cboSearch.Size = new System.Drawing.Size(244, 28);
+            this.cboSearch.TabIndex = 10;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Enabled = false;
+            this.btnSearch.Location = new System.Drawing.Point(1070, 143);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(163, 42);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // FormMap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1389, 681);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cboSearch);
+            this.Controls.Add(this.lblTargetVertex);
+            this.Controls.Add(this.lblStartVertex);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.panelGraph);
+            this.Name = "FormMap";
+            this.Text = "Cebu Map";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelGraph;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lblStartVertex;
+        private System.Windows.Forms.Label lblTargetVertex;
+        private System.Windows.Forms.ComboBox cboSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ CebuMap/Form.cs
@@ -0,0 +1,482 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CebuMap
+{
+    public partial class FormMap : Form
+    {
+        private Dictionary<int, Vertex> vertexList;
+        private int vertexCount;
+        private int[,] adjacencyMatrix;
+        private Vertex startVertex;
+        private Vertex targetVertex;
+        private Label[] vertexLabel;
+        private List<Vertex> pathList;
+
+        public FormMap()
+        {
+            InitializeComponent();
+            vertexList = new Dictionary<int, Vertex>();
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            bool isSearch = false;
+
+            // Breadth-First Search
+            if (cboSearch.SelectedIndex == 0)
+            {
+                BreadthFirstSearch(startVertex, targetVertex);
+                isSearch = true;
+            }
+            // Depthth-First Search
+            else if (cboSearch.SelectedIndex == 1)
+            {
+                DepthFirstSearch(startVertex, targetVertex);
+                isSearch = true;
+            }
+            // Greedy Best-First Search
+            else if (cboSearch.SelectedIndex == 2)
+            {
+                GreedyBestFirstSearch(startVertex, targetVertex);
+                isSearch = true;
+            }
+            // A* Search
+            else if (cboSearch.SelectedIndex == 3)
+            {
+                AStarSearch(startVertex, targetVertex);
+                isSearch = true;
+            }
+
+            if (isSearch)
+            {
+                cboSearch.Enabled = false;
+                btnSearch.Enabled = false;
+                btnRemove.Enabled = true;
+            }
+        }
+
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            AddVertices();
+            AddEdges();
+
+            //DisplayEdges();
+            DisplayVertices();
+            
+            btnDisplay.Enabled = false;
+        }
+
+        private void BtnRemove_Click(object sender, EventArgs e)
+        {
+            vertexLabel[startVertex.Index].ForeColor = Color.Red;
+            vertexLabel[startVertex.Index].BackColor = Color.Red;
+            vertexLabel[targetVertex.Index].ForeColor = Color.Red;
+            vertexLabel[targetVertex.Index].BackColor = Color.Red;
+
+            lblStartVertex.Text = "Start Vertex:";
+            lblTargetVertex.Text = "Target Vertex:";
+
+            startVertex = null;
+            targetVertex = null;
+            cboSearch.Enabled = false;
+            btnSearch.Enabled = false;
+            btnRemove.Enabled = false;
+
+            panelGraph.Refresh();
+            panelGraph.Invalidate();
+        }
+
+        private void AddVertices()
+        {
+            StreamReader sr = new StreamReader(Environment.CurrentDirectory + "//vertices.txt");
+            string line = null;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] temp = line.Split(',');
+                AddVertex(temp[2], Int32.Parse(temp[0]), Int32.Parse(temp[1]));
+            }
+            sr.Close();
+        }
+
+        private void AddEdges()
+        {
+            vertexLabel = new Label[vertexCount];
+            adjacencyMatrix = new int[vertexCount, vertexCount];
+
+            StreamReader sr = new StreamReader(Environment.CurrentDirectory + "//edges.txt");
+            string line = null; 
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] temp = line.Split(',');
+                int v1 = GetVertexIndex(temp[0]);
+                int v2 = GetVertexIndex(temp[1]);
+                AddEdge(vertexList[v1], vertexList[v2]);
+            }
+            sr.Close();
+        }
+
+        public void AddVertex(string name, int x, int y)
+        {
+            vertexList.Add(vertexCount, new Vertex(name, vertexCount, x, y));
+            vertexCount++;
+        }
+
+        public void AddEdge(Vertex v1, Vertex v2)
+        {
+            if (v1.Index == v2.Index)
+            {
+                Console.WriteLine("Not a valid edge");
+            }
+            else
+            {
+                if (adjacencyMatrix[v1.Index, v2.Index] != 0)
+                {
+                    Console.WriteLine("Edge is already present.");
+                }
+                else
+                {
+                    adjacencyMatrix[v1.Index, v2.Index] = GetDistance(v1, v2);
+                    adjacencyMatrix[v2.Index, v1.Index] = adjacencyMatrix[v1.Index, v2.Index];
+                }
+            }
+        }
+
+        public void DisplayEdges()
+        {
+            for (int i = 0; i < vertexCount; i++)
+            {
+                for (int j = 0; j < vertexCount; j++)
+                {
+                    if (adjacencyMatrix[i, j] != 0)
+                    {
+                        Point point1 = new Point(vertexList[i].X + 4, vertexList[i].Y + 4);
+                        Point point2 = new Point(vertexList[j].X + 4, vertexList[j].Y + 4);
+                        Pen pen = new Pen(Color.LightGray, 1);
+                        Graphics graphics = panelGraph.CreateGraphics();
+                        graphics.DrawLine(pen, point1, point2);
+                        pen.Dispose();
+                        graphics.Dispose();
+                    }
+                }
+            }
+        }
+
+        public void DisplayVertices()
+        {
+            foreach (KeyValuePair<int, Vertex> entry in vertexList)
+            {
+                vertexLabel[entry.Key] = new Label
+                {
+                    Width = 7,
+                    Height = 7,
+                    AutoSize = false,
+                    Location = new Point(entry.Value.X, entry.Value.Y),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    ForeColor = Color.Red,
+                    BackColor = Color.Red,
+                    Text = entry.Value.Index.ToString()
+                };
+                vertexLabel[entry.Key].Click += new EventHandler(Vertex_Click);
+                panelGraph.Controls.Add(vertexLabel[entry.Key]);
+            }
+        }
+
+        public void Vertex_Click(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+            int vertexIndex = Int32.Parse(label.Text);
+
+            if (null == startVertex)
+            {
+                cboSearch.Enabled = false;
+                btnSearch.Enabled = false;
+                btnRemove.Enabled = false;
+                label.ForeColor = Color.Green;
+                label.BackColor = Color.Green;
+                startVertex = vertexList[vertexIndex];
+                lblStartVertex.Text = "Start Vertex: " + startVertex.X + ", " + startVertex.Y;
+            }
+            else if (null == targetVertex)
+            {
+                cboSearch.Enabled = true;
+                btnSearch.Enabled = true;
+                btnRemove.Enabled = true;
+                label.ForeColor = Color.Green;
+                label.BackColor = Color.Green;
+                targetVertex = vertexList[vertexIndex];
+                lblTargetVertex.Text = "Target Vertex: " + targetVertex.X + ", " + targetVertex.Y;
+            }
+            else
+            {
+                cboSearch.Enabled = true;
+                btnSearch.Enabled = true;
+                btnRemove.Enabled = true;
+            }
+        }
+
+        public int GetVertexIndex(String vertexName)
+        {
+            foreach (KeyValuePair<int, Vertex> entry in vertexList)
+            {
+                if (vertexName.Equals(entry.Value.Name))
+                {
+                    return entry.Key;
+                }
+            }
+            return -1;
+        }
+
+        public int GetDistance(Vertex v1, Vertex v2)
+        {
+            double x = Math.Pow(v2.X - v1.X, 2);
+            double y = Math.Pow(v2.Y - v1.Y, 2);
+            return (int) Math.Round(Math.Pow((x + y), 0.5));
+        }
+
+        
+        public void BreadthFirstSearch(Vertex startVertex, Vertex targetVertex)
+        {
+            bool[] visited = new bool[vertexCount];
+
+            Queue<Vertex> queue = new Queue<Vertex>();
+            List<string> pathList = new List<string>();
+            
+            visited[startVertex.Index] = true;
+            queue.Enqueue(startVertex);
+
+            while (queue.Count > 0)
+            {
+                Vertex vertex = queue.Dequeue();
+                pathList.Add(vertex.Name);
+
+                if (vertex.Name.Equals(targetVertex.Name))
+                {
+                    RetracePath(startVertex, targetVertex);
+                    break;
+                }
+
+                List<Vertex> neighbors = GetNeighbors(vertex).OrderBy(e => e.Distance).ThenBy(e => e.Name).ToList();
+
+                foreach (Vertex neighbor in neighbors)
+                {
+                    if (!visited[neighbor.Index])
+                    {
+                        Point point1 = new Point(vertex.X + 3, vertex.Y + 3);
+                        Point point2 = new Point(neighbor.X + 3, neighbor.Y + 3);
+                        Pen pen = new Pen(Color.Blue, 1);
+                        Graphics graphics = panelGraph.CreateGraphics();
+                        graphics.DrawLine(pen, point1, point2);
+                        pen.Dispose();
+                        graphics.Dispose();
+
+                        visited[neighbor.Index] = true;
+                        neighbor.Parent = vertex;
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+        }
+        
+        public void DepthFirstSearch(Vertex startVertex, Vertex targetVertex)
+        {
+            bool[] visited = new bool[vertexCount];
+
+            Stack<Vertex> stack = new Stack<Vertex>();
+            List<string> pathList = new List<string>();
+
+            visited[startVertex.Index] = true;
+            stack.Push(startVertex);
+
+            while (stack.Count > 0)
+            {
+                Vertex vertex = stack.Pop();
+                pathList.Add(vertex.Name);
+
+                if (vertex.Name.Equals(targetVertex.Name))
+                {
+                    RetracePath(startVertex, targetVertex);
+                    break;
+                }
+
+                List<Vertex> neighbors = GetNeighbors(vertex).OrderByDescending(e => e.Distance).ThenBy(e => e.Name).ToList();
+
+                foreach (Vertex neighbor in neighbors)
+                {
+                    if (!visited[neighbor.Index])
+                    {
+                        Point point1 = new Point(vertex.X + 3, vertex.Y + 3);
+                        Point point2 = new Point(neighbor.X + 3, neighbor.Y + 3);
+                        Pen pen = new Pen(Color.Blue, 1);
+                        Graphics graphics = panelGraph.CreateGraphics();
+                        graphics.DrawLine(pen, point1, point2);
+                        pen.Dispose();
+                        graphics.Dispose();
+
+                        visited[neighbor.Index] = true;
+                        neighbor.Parent = vertex;
+                        stack.Push(neighbor);
+                    }
+                }
+            }
+        }
+
+        public void GreedyBestFirstSearch(Vertex startVertex, Vertex targetVertex)
+        {
+            List<Vertex> openVertices = new List<Vertex>();
+            HashSet<Vertex> closedVertices = new HashSet<Vertex>();
+            openVertices.Add(startVertex);
+
+            while (openVertices.Count > 0)
+            {
+                Vertex vertex = openVertices[0];
+
+                for (int i = 1; i < openVertices.Count; i++)
+                {
+                    if (openVertices[i].FCost < vertex.FCost)
+                    {
+                        vertex = openVertices[i];
+                    }
+                }
+
+                openVertices.Remove(vertex);
+                closedVertices.Add(vertex);
+
+                if (vertex == targetVertex)
+                {
+                    RetracePath(startVertex, targetVertex);
+                    return;
+                }
+
+                foreach (Vertex neighbor in GetNeighbors(vertex))
+                {
+                    if (closedVertices.Contains(neighbor))
+                    {
+                        continue;
+                    }
+
+                    if (!openVertices.Contains(neighbor))
+                    {
+                        Point point1 = new Point(vertex.X + 3, vertex.Y + 3);
+                        Point point2 = new Point(neighbor.X + 3, neighbor.Y + 3);
+                        Pen pen = new Pen(Color.Blue, 1);
+                        Graphics graphics = panelGraph.CreateGraphics();
+                        graphics.DrawLine(pen, point1, point2);
+                        pen.Dispose();
+                        graphics.Dispose();
+
+                        neighbor.HCost = GetDistance(neighbor, targetVertex);
+                        neighbor.Parent = vertex;
+                        openVertices.Add(neighbor);
+                    }
+                }
+            }
+        }
+
+        public void AStarSearch(Vertex startVertex, Vertex targetVertex)
+        {
+            List<Vertex> openVertices = new List<Vertex>();
+            HashSet<Vertex> closedVertices = new HashSet<Vertex>();
+            openVertices.Add(startVertex);
+
+            while (openVertices.Count > 0)
+            {
+                Vertex vertex = openVertices[0];
+
+                for (int i = 1; i < openVertices.Count; i++)
+                {
+                    if (openVertices[i].FCost <= vertex.FCost)
+                    {
+                        if (openVertices[i].HCost < vertex.HCost)
+                        {
+                            vertex = openVertices[i];
+                        }
+                    }
+                }
+
+                openVertices.Remove(vertex);
+                closedVertices.Add(vertex);
+
+                if (vertex == targetVertex)
+                {
+                    RetracePath(startVertex, targetVertex);
+                    return;
+                }
+
+                List<Vertex> neighbors = GetNeighbors(vertex).OrderByDescending(e => e.Distance).ThenBy(e => e.Name).ToList();
+
+                foreach (Vertex neighbor in neighbors)
+                {
+                    if (closedVertices.Contains(neighbor))
+                    {
+                        continue;
+                    }
+
+                    int newCostToNeighbour = vertex.GCost + GetDistance(vertex, neighbor);
+                    if (newCostToNeighbour < neighbor.GCost || !openVertices.Contains(neighbor))
+                    {
+                        neighbor.GCost = newCostToNeighbour;
+                        neighbor.HCost = GetDistance(neighbor, targetVertex);
+                        neighbor.Parent = vertex;
+
+                        if (!openVertices.Contains(neighbor))
+                        {
+                            Point point1 = new Point(vertex.X + 3, vertex.Y + 3);
+                            Point point2 = new Point(neighbor.X + 3, neighbor.Y + 3);
+                            Pen pen = new Pen(Color.Blue, 1);
+                            Graphics graphics = panelGraph.CreateGraphics();
+                            graphics.DrawLine(pen, point1, point2);
+                            pen.Dispose();
+                            graphics.Dispose();
+
+                            openVertices.Add(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+
+        public List<Vertex> GetNeighbors(Vertex parentVertex)
+        {
+            List<Vertex> neighbors = new List<Vertex>();
+
+            for (int i = 0; i < vertexCount; i++)
+            {
+                if (adjacencyMatrix[parentVertex.Index, i] != 0)
+                {
+                    Vertex vertex = vertexList[i];
+                    vertex.Distance = GetDistance(parentVertex, vertex);
+                    neighbors.Add(vertex);
+                }
+            }
+
+            return neighbors;
+        }
+
+        public void RetracePath(Vertex startVertex, Vertex endVertex)
+        {
+            List<Vertex> path = new List<Vertex>();
+            Vertex currentVertex = endVertex;
+
+            while (currentVertex != startVertex)
+            {
+                path.Add(currentVertex);
+                Point point1 = new Point(currentVertex.X + 3, currentVertex.Y + 3);
+                Point point2 = new Point(currentVertex.Parent.X + 3, currentVertex.Parent.Y + 3);
+                Pen pen = new Pen(Color.Green, 2);
+                Graphics graphics = panelGraph.CreateGraphics();
+                graphics.DrawLine(pen, point1, point2);
+                pen.Dispose();
+                graphics.Dispose();
+                currentVertex = currentVertex.Parent;
+            }
+            path.Add(startVertex);
+            path.Reverse();
+            pathList = path;
+        }
+    }
+}
